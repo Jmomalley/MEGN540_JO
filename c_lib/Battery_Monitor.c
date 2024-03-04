cd4ede04c41e1a0597891a566247a765dfc17603
Jmomalley <122636503+Jmomalley@users.noreply.github.com>
2024-03-03 18:52:04 -0700

    Update Battery_Monitor.c

--- c_lib/Battery_Monitor.c
+++ c_lib/Battery_Monitor.c
@@ -1,6 +1,8 @@
 #include "Battery_Monitor.h"
 
-static const float BITS_TO_BATTERY_VOLTS = 0.0f;
+#include "led_interface.h"
+
+static const float BITS_TO_BATTERY_VOLTS = 0.0050048876f;  // ((ADC * V_ref )/ (2^10 - 1))*2
 
 /**
  * Function Initialize_Battery_Monitor initializes the Battery Monitor to record the current battery voltages.
@@ -8,7 +10,23 @@ static const float BITS_TO_BATTERY_VOLTS = 0.0f;
 void Initialize_Battery_Monitor()
 {
 
-    // *** MEGN540 LAB3 YOUR CODE HERE ***
+    // PORTF |= ( 1 << PORTF6 );  // PF6
+
+    // DDRF &= ( 1 << DDE6 );  // INPUT
+
+    ADCSRA |= ( 1 << ADEN );  // Enable the ADC
+
+    ADCSRA |= ( 1 << ADPS2 ) | ( 1 << ADPS1 ) | ( 1 << ADPS0 );  // Set the prescaler to 128
+
+    ADMUX |= ( 1 << MUX1 ) | ( 1 << MUX2 );  // Select ADC6 which is connected to PF6
+
+    ADCSRB &= ( 0 << MUX5 );
+
+    ADMUX |= ( 1 << REFS0 ) | ( 1 << REFS1 );  // Sets the Reference Voltage to 2.56V
+
+    // ADMUX |= ( 1 << REFS0 );
+
+    DIDR0 |= ( 1 << ADC6D );  // Disable digital inputs on ADC6
 }
 
 /**
@@ -26,6 +44,14 @@ float Battery_Voltage()
     } data = { .value = 0 };
 
     // *** MEGN540 LAB3 YOUR CODE HERE ***
+    ADCSRA |= ( 1 << ADSC );  // Start the ADC measurement
+
+    while( !( ADCSRA & ( 1 << ADIF ) ) ) {};  // wait for ADC to complete reading
+
+    data.split.LSB = ADCL;  // Read the low bits
+    data.split.MSB = ADCH;  // Read the high bits
+
+    // ADCSRA |= ( 1 << ADIF );
 
     return data.value * BITS_TO_BATTERY_VOLTS;
 }
